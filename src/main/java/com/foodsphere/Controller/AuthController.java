08a192f84e61cc66c4f24b1d748c2f53cecca754
vishaldange <vishaldange2002@gmail.com>
2024-08-01 13:41:12 +0530

    Implement SignUp and SignIn Methods

---
+++ src/main/java/com/foodsphere/Controller/AuthController.java
@@ -0,0 +1,119 @@
+package com.foodsphere.Controller;
+
+import com.foodsphere.config.JwtProvider;
+import com.foodsphere.model.Cart;
+import com.foodsphere.model.USER_ROLE;
+import com.foodsphere.model.User;
+import com.foodsphere.repository.CartRepository;
+import com.foodsphere.repository.UserRepository;
+import com.foodsphere.request.LoginRequest;
+import com.foodsphere.response.AuthResponse;
+import com.foodsphere.service.CustomerUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtProvider jwtProvider;
+
+    @Autowired
+    private CustomerUserDetailsService customerUserDetailsService;
+
+    @Autowired
+    private CartRepository cartRepository;
+
+
+    @PostMapping("/signUp")
+    public ResponseEntity<AuthResponse> createUserHandler(@RequestBody User user) throws Exception {
+
+        User isEmailExists=userRepository.findByEmail(user.getEmail());
+        if(isEmailExists!=null){
+            throw new Exception("User exists with same email is. Please try with other email");
+        }
+
+        User createUser=new User();
+
+        createUser.setEmail(user.getEmail());
+        createUser.setFullName(user.getFullName());
+        createUser.setRole(user.getRole());
+        createUser.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        User savedUser=userRepository.save(createUser);
+
+
+        Cart cart=new Cart();
+        cart.setCustomer(savedUser);
+        cartRepository.save(cart);
+
+        Authentication authentication=new UsernamePasswordAuthenticationToken(user.getEmail(),user.getPassword());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt=jwtProvider.generateToken(authentication);
+
+        AuthResponse authResponse=new AuthResponse();
+        authResponse.setJwt(jwt);
+        authResponse.setMessage("Register success");
+        authResponse.setRole(savedUser.getRole());
+
+        return new ResponseEntity<>(authResponse, HttpStatus.CREATED);
+    }
+
+
+    @PostMapping("/signIn")
+    public ResponseEntity<AuthResponse> signIn(@RequestBody LoginRequest req){
+
+        String username=req.getEmail();
+        String password=req.getPassword();
+
+        Authentication authentication=authenticate(username,password);
+        Collection<?extends GrantedAuthority> authorities=authentication.getAuthorities();
+
+        String role=authorities.isEmpty()?null:authorities.iterator().next().getAuthority();
+
+        String jwt=jwtProvider.generateToken(authentication);
+
+        AuthResponse authResponse=new AuthResponse();
+        authResponse.setJwt(jwt);
+        authResponse.setMessage("Register success");
+        authResponse.setRole(USER_ROLE.valueOf(role));
+
+        return new ResponseEntity<>(authResponse, HttpStatus.OK);
+    }
+
+    private Authentication authenticate(String username, String password) {
+
+        UserDetails userDetails=customerUserDetailsService.loadUserByUsername(username);
+
+        if(userDetails==null){
+            throw new BadCredentialsException("Invalid Username..");
+        }
+        if(!passwordEncoder.matches(password,userDetails.getPassword())){
+            throw new BadCredentialsException("Invalid password...");
+        }
+
+        return new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+    }
+}
---
+++ src/main/java/com/foodsphere/response/AuthResponse.java
@@ -0,0 +1,15 @@
+package com.foodsphere.response;
+
+import com.foodsphere.model.USER_ROLE;
+import lombok.Data;
+
+@Data
+public class AuthResponse {
+
+    private String jwt;
+
+    private String message;
+
+    private USER_ROLE role;
+
+}
