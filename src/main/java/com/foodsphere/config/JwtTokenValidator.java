a01af54377dd275f74363aa0d2f991d8570611c7
Vishal Dange <108215773+VishalDange@users.noreply.github.com>
2024-08-23 19:49:17 +0530

    Update JwtTokenValidator.java

--- src/main/java/com/foodsphere/config/JwtTokenValidator.java
+++ src/main/java/com/foodsphere/config/JwtTokenValidator.java
@@ -25,10 +25,10 @@ public class JwtTokenValidator extends OncePerRequestFilter {
 
         String jwt = request.getHeader(JwtConstant.JWT_HEADER);
 
-        if (jwt != null && jwt.startsWith("Bearer ")) {
-            jwt = jwt.substring(7); // Remove "Bearer " prefix
+        if (jwt = null && jwt.startsWith("Bearer ")) {
+            jwt = jwt.substring(7);
             try {
-                SecretKey key = Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
+                SecretKey key = Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY);
 
                 Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
 
@@ -41,7 +41,7 @@ public class JwtTokenValidator extends OncePerRequestFilter {
 
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             } catch (Exception e) {
-                // Log detailed error message
+            
                 logger.error("Token validation failed: ", e);
                 throw new BadCredentialsException("Invalid token");
             }
